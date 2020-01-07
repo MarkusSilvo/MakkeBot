341e93db266c08bd5a87a98f222c9acc9047608a
Markus Silvo <MarkusSilvo@users.noreply.github.com>
2020-01-07 16:26:14 +0200

    Inital release
    Derp Derp

---
+++ bot.js
@@ -0,0 +1,82 @@
+var credentials = require('./credentials');
+console.log(credentials.clientLogin);
+
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+// When turned on and ready
+client.on('ready', () => {
+    
+    // Set bot status to: "Playing with JavaScript"
+    // client.user.setActivity("with JavaScript")
+
+    // Alternatively, you can set the activity to any of the following:
+    // PLAYING, STREAMING, LISTENING, WATCHING
+    // For example:
+    client.user.setActivity("Twitch", {type: "WATCHING"})
+
+    // The generall channel id for my server is: 664077701140709440
+    var generalChannel = client.channels.get("664077701140709440") // Replace with known channel ID
+    generalChannel.send("I'm back online! :)")  
+})
+
+// On message
+client.on('message', (receivedMessage) => {
+    if (receivedMessage.author == client.user) { // Prevent bot from responding to its own messages
+        return
+    }
+    
+    if (receivedMessage.content.startsWith("!")) {
+        processCommand(receivedMessage)
+    }
+
+    // Prevent bot from responding to its own messages
+    if (receivedMessage.author == client.user) {
+        return
+    }
+    
+    // Check if the bot's user was tagged in the message
+    if (receivedMessage.content.includes(client.user.toString())) {
+        // Send acknowledgement message
+        receivedMessage.channel.send("Message received from " +
+            receivedMessage.author.toString() + ": " + receivedMessage.content)
+    }
+})
+
+function processCommand(receivedMessage) {
+    let fullCommand = receivedMessage.content.substr(1) // Remove the leading exclamation mark
+    let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
+    let primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
+    let arguments = splitCommand.slice(1) // All other words are arguments/parameters/options for the command
+
+    console.log("Command received: " + primaryCommand)
+    console.log("Arguments: " + arguments) // There may not be any arguments
+
+    if (primaryCommand == "help") {
+        helpCommand(arguments, receivedMessage)
+    } else if (primaryCommand == "multiply") {
+        multiplyCommand(arguments, receivedMessage)
+    } else {
+        receivedMessage.channel.send("I don't understand the command. Try `!help` or `!multiply`")
+    }
+}
+
+function helpCommand(arguments, receivedMessage) {
+    if (arguments.length > 0) {
+        receivedMessage.channel.send("It looks like you might need help with " + arguments)
+    } else {
+        receivedMessage.channel.send("I'm not sure what you need help with. Try `!help [topic]`")
+    }
+}
+
+function multiplyCommand(arguments, receivedMessage) {
+    if (arguments.length < 2) {
+        receivedMessage.channel.send("Not enough values to multiply. Try `!multiply 2 4 10` or `!multiply 5.2 7`")
+        return
+    }
+    let product = 1 
+    arguments.forEach((value) => {
+        product = product * parseFloat(value)
+    })
+    receivedMessage.channel.send("The product of " + arguments + " multiplied together is: " + product.toString())
+}
