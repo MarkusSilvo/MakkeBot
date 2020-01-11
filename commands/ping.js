a01c8dad38b75705f43bc27a1e25e186116e2ad6
JokkeeZ <11576776+JokkeeZ@users.noreply.github.com>
2020-01-11 12:58:28 +0200

    Added !ping command

---
+++ commands/ping.js
@@ -0,0 +1,23 @@
+/**
+ * Command information.
+ */
+const info = {
+	name: 'ping',
+	desc: 'Used to check if bot is alive'
+};
+
+/**
+ * Sends pong back to user.
+ * 
+ * @param {Discord.Client} client Bot client
+ * @param {string[]} arguments Command arguments
+ * @param {Discord.Message} message Message that contained the command.
+ */
+const execute = (client, arguments, message) => {
+	// Get MakkeBot emote
+	let botEmote = client.emojis.find(emoji => emoji.name === 'MakkeBot');
+	// Reply to user.
+	message.reply(`Pong! ${botEmote != null ? botEmote : ''}`);
+};
+
+module.exports = { info: info, execute: execute };
